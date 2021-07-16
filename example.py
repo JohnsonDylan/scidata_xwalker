842e89d1337e7be1b1385b74e266f8441ab4fba2
Dylan <N01448636@UNF.edu>
2021-07-16 10:52:18 -0400

    Initial commit

---
+++ example.py
@@ -0,0 +1,131 @@
+from scidata_crosswalker import *
+from scidata import *
+import json
+import datetime
+
+
+merged_dictionary = \
+    {"compounds": {
+        "id": 596,
+        "dsstox_compound_id": "DTXCID10596",
+        "smiles": "CCC1=CC=CC=C1",
+        "jchem_inchi_key": "YNQLUTRBYVCPMQ-UHFFFAOYSA-N",
+        "acd_iupac_name": "Ethylbenzene",
+        "mol_formula": "C8H10",
+        "mol_weight": 106.168,
+        "qsar_predicted_properties": [{
+                "id": 5909,
+                "efk_dsstox_compound_id": 596,
+                "efk_qsar_model_id": 14,
+                "result_value": 130.52,
+                "report_filename": "DTXCID10596-TEST_Thermal_Conductivity.html",
+                "models": {
+                    "id": 14,
+                    "efk_chemprop_endpoint_id": 15,
+                    "name": "TEST_Thermal_Conductivity"
+                }},{
+                "id": 5982,
+                "efk_dsstox_compound_id": 596,
+                "efk_qsar_model_id": 4,
+                "result_value": 0.89,
+                "report_filename": "DTXCID10596-TEST_Density.html",
+                "models": {
+                    "id": 4,
+                    "efk_chemprop_endpoint_id": 5,
+                    "name": "TEST_Density",
+                }}]}}
+
+object_data_json = json.loads(json.dumps(merged_dictionary))
+
+central_table = 'compounds'
+crosswalks = [{'id': 16, 'table': 'compounds', 'field': 'dsstox_compound_id', 'ontterm_id': 323, 'sdsection': 'system', 'sdsubsection': 'compound', 'sdsubsubsection': 'identifier', 'category': None, 'unit': None, 'datatype': 'string'},
+              {'id': 23, 'table': 'compounds', 'field': 'smiles', 'ontterm_id': 80, 'sdsection': 'system', 'sdsubsection': 'compound', 'sdsubsubsection': 'identifier', 'category': None, 'unit': None, 'datatype': 'string'},
+              {'id': 47, 'table': 'qsar_predicted_properties', 'field': 'result_value', 'ontterm_id': 9999, 'sdsection': 'dataset', 'sdsubsection': 'exptdata', 'sdsubsubsection': None, 'category': None, 'unit': None, 'datatype': 'string'},
+              {'id': 48, 'table': 'models', 'field': 'name', 'ontterm_id': 9999, 'sdsection': 'methodology', 'sdsubsection': 'model', 'sdsubsubsection': '', 'category': None, 'unit': None, 'datatype': 'string'},
+              {'id': 49, 'table': 'qsar_predicted_properties', 'field': 'report_filename', 'ontterm_id': 9999, 'sdsection': 'dataset', 'sdsubsection': 'suppdata', 'sdsubsubsection': None, 'category': None, 'unit': None, 'datatype': 'string'}]
+
+onttermslist = [{'id': 323, 'title': 'Identifier', 'definition': 'One or more characters used to identify, name, or characterize the nature, properties, or contents of a thing. [def-source: NCI]', 'code': 'NCIT_C25364', 'url': 'obo:NCIT_C25364', 'nspace_id': 2, 'sdsection': 'metadata', 'sdsubsection': 'identifier'},
+                {'id': 80, 'title': 'SMILES string', 'definition': 'SMILES string corresponding to drug structure [database_cross_reference: PMID:14755292]', 'code': 'MI:2039', 'url': 'obo:MI_2039', 'nspace_id': 2, 'sdsection': 'system', 'sdsubsection': 'compound'},
+                {'id': 9999, 'title': 'TEST', 'definition': 'TEST', 'code': 'test_9999', 'url': 'test:test_9999', 'nspace_id': 19, 'sdsection': 'system', 'sdsubsection': ''}]
+
+nspaceslist = [{'id': 2, 'name': 'OBO Foundry', 'ns': 'obo', 'path': 'http://purl.obolibrary.org/obo/', 'homepage': 'http://www.ontobee.org'},
+               {'id': 19, 'name': 'SciData', 'ns': 'sdo', 'path': 'https://stuchalk.github.io/scidata/ontology/scidata.owl#', 'homepage': 'https://stuchalk.github.io/scidata/'}]
+
+
+"""Define group overrides to modify organization
+    term 1 = < regex pattern to find in value # key >
+    term 2 = < regex pattern to find in value of # for match >
+    { term 1 : term 2 }
+    If term 1 depends on enumeration use parentheses to create regex groups. Group 2 should be the enumerated value ie. (\d{1,}) 
+    If term 2 match depends on the enumeration, use '$!@%' in the position of enumeration
+    """
+group_overrides = {}
+group_overrides.update({'(compounds;qsar_predicted_properties;)(\d{1,})(\/exptdata)':'compounds;qsar_predicted_properties;$!@%/data'})
+group_overrides.update({'(compounds;qsar_predicted_properties;)(\d{1,})(\/suppdata)':'compounds;qsar_predicted_properties;$!@%/data'})
+
+"""Define sci_links to create internal links between sections
+    term 1 = < regex pattern to find in value # key >
+    term 2 = < name of key to be added to identify relationship >
+    term 3 = < regex pattern to find in value of # for match >
+    { term 1 : { term 2 : term 3 } }
+    If term 1 depends on enumeration use parentheses to create regex groups. Group 2 should be the enumerated value ie. (\d{1,}) 
+    If term 3 match depends on the enumeration, use '$!@%' in the position of enumeration
+    Multiple key/value pairs can be included in term 3 if needed
+    """
+sci_links = {}
+sci_links.update({'(compounds;qsar_predicted_properties;)(\d{1,})(\/data)': {
+    'model':'compounds;qsar_predicted_properties;$!@%/model',
+    'compound':'compounds/compound'
+    }})
+
+uid = '596'
+
+""""""
+""""""
+
+"""Initialize SciData object"""
+test = SciData(uid)
+
+crosswalker(central_table, object_data_json, crosswalks, 'field', False)
+
+flat_object_data_json = flatten_json_iterative_solution(object_data_json)
+
+object_data_json_flat_filtered = cleanup_flattened(flat_object_data_json)
+
+group_link_override(object_data_json_flat_filtered, group_overrides)
+
+nspaces = get_semantics(object_data_json_flat_filtered, onttermslist, nspaceslist)
+
+bins = binner(object_data_json_flat_filtered)
+
+bins_grouped = bin_grouper(bins)
+
+datasetmod = datasetmodder(bins_grouped['dataset'])
+
+remove_extra_metadata(bins_grouped['methodology'])
+
+remove_extra_metadata(bins_grouped['system'])
+
+test.aspects(bins_grouped['methodology'])
+test.facets(bins_grouped['system'])
+test.datapoint(datasetmod)
+
+test.namespaces(nspaces)
+test.base("https://scidata.unf.edu/example/")
+test.discipline('w3i:Chemistry')
+test.subdiscipline('w3i:ChemicalInformatics')
+test.docid('1')
+test.version('1')
+test.title('SciData Crosswalker')
+test.author(['Dylan Johnson'])
+test.description('Example of Crosswalker Functions')
+test.publisher('Chalk Lab')
+test.permalink("https://scidata.unf.edu/example/")
+
+scilinker(test.output, sci_links)
+
+# print(json.dumps(test.output, ensure_ascii=False))
+
+scicleanup(test.output)
+
+print(json.dumps(test.output, ensure_ascii=False))
---
+++ scidata_crosswalker.py
@@ -0,0 +1,293 @@
+from itertools import chain, starmap
+import ast
+import re
+
+dev = False
+
+
+def crosswalker(sci_dir, sci_input, crosswalks, cw_field, cw_table):
+    """Match directory/table name and field to a crosswalk entry.
+    If match exists, modify value to a string representation of a
+    dictionary containing the value and crosswalk information """
+    for x, y in sci_input.items():
+        if type(y) in [str, int, float]:
+            if cw_table:
+                match = next((cw for cw in crosswalks if cw[cw_field] == x and cw[cw_table] == sci_dir), None)
+            else:
+                match = next((cw for cw in crosswalks if cw[cw_field] == x), None)
+            if match:
+                out = {"scidata_value": y}
+                out.update(match)
+                out.pop('updated', None)
+                sci_input[x] = str(out)
+        if type(y) == dict:
+            crosswalker(x, y, crosswalks, cw_field, cw_table)
+        if type(y) == list:
+            for z in y:
+                crosswalker(x, z, crosswalks, cw_field, cw_table)
+
+
+def flatten_json_iterative_solution(dictionary):
+    """Flatten a nested json file"""
+
+    def unpack(parent_key, parent_value):
+        """Unpack one level of nesting in json file"""
+        # Unpack one level only!!!
+
+        if isinstance(parent_value, dict):
+            for key, value in parent_value.items():
+                temp1 = parent_key + ';' + key
+                yield temp1, value
+        elif isinstance(parent_value, list):
+            i = 0
+            for value in parent_value:
+                temp2 = parent_key + ';' + str(i)
+                i += 1
+                yield temp2, value
+        else:
+            yield parent_key, parent_value
+
+            # Keep iterating until the termination condition is satisfied
+
+    while True:
+        # Keep unpacking the json file until all values are atomic elements (not dictionary or list)
+        dictionary = dict(chain.from_iterable(starmap(unpack, dictionary.items())))
+        # Terminate condition: not any value in the json file is dictionary or list
+        if not any(isinstance(value, dict) for value in dictionary.values()) and \
+                not any(isinstance(value, list) for value in dictionary.values()):
+            break
+
+    return dictionary
+
+
+def cleanup_flattened(sci_input):
+    """Converts stringified dictionary values to literal dicts.
+    Also populates scidata_dir, scidata_key, scidata_group"""
+    output = {}
+    for k, v in sci_input.items():
+        if isinstance(v, str) and v is not None and v.startswith('{'):
+            if re.search(r'(\d+)(?!.*\d)', k):
+                reg = re.split(r'(\d+)(?!.*\d)', k)
+                reg.pop()
+                sci_group = ''.join([str(elem) for elem in reg])
+            else:
+                sci_group = k.rsplit(';', 1)[0]
+
+            if re.search(r'^(\D*\d)(.*)$', k):
+                reg = re.search(r'^(\D*\d)(.*)$', k)
+                sci_group_alt = reg.group(1)
+            else:
+                sci_group_alt = k.rsplit(';', 1)[0]
+            val = ({'scidata_dir': k, 'scidata_key': k.rsplit(';', 1)[1], 'scidata_group': sci_group,
+                    'scidata_group_alt': sci_group_alt})
+            val.update(ast.literal_eval(v))
+            val.update({'scidata_group_link': sci_group + '/' + str(val['sdsubsection'])})
+            output.update({k: val})
+    return output
+
+
+def group_link_override(sci_input, group_overrides):
+    """redefines the scidata_group_link value based on the rules defined in group_overrides"""
+    for k, v in sci_input.items():
+        for pattern, replacement in group_overrides.items():
+            if re.search(pattern, v['scidata_group_link']):
+                try:
+                    replace = replacement.replace('$!@%', re.match(pattern, v['scidata_group_link']).group(2))
+                    v['scidata_group_link'] = replace
+                except Exception:
+                    v['scidata_group_link'] = replacement
+
+
+def get_semantics(sci_input, onttermslist, nspaceslist):
+    """Scans object_data_json_flat_filtered to create list of all ontterms present"""
+    ontoterms = []
+    namespaces = {}
+    for x in sci_input.values():
+        ontoterms.append(x['ontterm_id'])
+        ontsearch = next(item for item in onttermslist if item["id"] == x['ontterm_id'])
+        nspacesearch = next(item for item in nspaceslist if item["id"] == ontsearch['nspace_id'])
+        namespaces.update({nspacesearch['ns']: nspacesearch['path']})
+    return namespaces
+
+
+def binner(sci_input):
+    """Separates into the three SciData Bins System/Methodology/Dataset"""
+    bins = {'methodology': [], 'system': [], 'dataset': []}
+    for k, v in sci_input.items():
+        bins[v['sdsection']].append(v)
+    return bins
+
+
+def bin_grouper(sci_input):
+    """Groups bins based on the 'scidata_group_link' key"""
+    bins_grouped = {}
+    for a, b in sci_input.items():
+        bin_groups_set = set()
+        for c in b:
+            bin_groups_set.add(c['scidata_group_link'])
+        subset_list = []
+        for d in bin_groups_set:
+            sdsubsection_group = {}
+            for c in b:
+                if c['scidata_group_link'] == d:
+                    if c['sdsection'] == 'dataset':
+                        if sdsubsection_group.get(c['scidata_key']):
+                            if type(sdsubsection_group.get(c['scidata_key'])) is dict:
+                                sdsubsection_group[c['scidata_key']] = list(sdsubsection_group.get(c['scidata_key']))
+                            sdsubsection_group[c['scidata_key']].append(c)
+                        else:
+                            sdsubsection_group.update(
+                                {"@id": 'datum', c['scidata_key']: c})
+                    else:
+                        if sdsubsection_group.get(c['scidata_key']):
+                            if type(sdsubsection_group.get(c['scidata_key'])) is dict:
+                                makelist = [sdsubsection_group.get(c['scidata_key'])]
+                                sdsubsection_group[c['scidata_key']] = makelist
+                            sdsubsection_group[c['scidata_key']].append(c)
+                        else:
+                            sdsubsection_group.update(
+                                {"@id": c['sdsubsection'], c['scidata_key']: c})
+            subset_list.append(sdsubsection_group)
+        if subset_list:
+            bins_grouped.update({a: subset_list})
+    return bins_grouped
+
+
+def remove_extra_metadata(sci_input):
+    """The dictionary containing the value and metadata is replaced with only the value (scidata_value)"""
+    for entry in sci_input:
+        reference = {}
+        for k, v in entry.items():
+            if type(v) is dict:
+                entry[k] = v['scidata_value']
+                reference.update({'#': v['scidata_group_link']})
+                if dev:
+                    reference.update({k + '#': v['scidata_group_link']})
+                    reference.update({k + '##': v['scidata_dir']})
+            if type(v) is list:
+                entry[k] = []
+                for vlist in v:
+                    entry[k].append(vlist['scidata_value'])
+                    reference.update({'#': vlist['scidata_group_link']})
+                    if dev:
+                        reference.update({k + '#': vlist['scidata_group_link']})
+                        reference.update({k + '##': vlist['scidata_dir']})
+                entry[k] = set(entry[k])
+                entry[k] = list(entry[k])
+                if len(entry[k]) == 1:
+                    entry[k] = entry[k][0]
+        entry.update(reference)
+
+
+def datasetmodder(sci_input):
+    """Functions for distributing data amongst datum and value sections"""
+    datasetmod = []
+    for x in sci_input:
+        datumtypes = set()
+        for k, v in x.items():
+            if k != '@id':
+                datumtypes.add(v['sdsubsection'])
+        datumset = []
+        for sdsub in datumtypes:
+            value = {}
+            for k, v in x.items():
+                if k != '@id':
+                    if v['sdsubsection'] == sdsub:
+                        value.update({"@id": "value",
+                                      "@type": "sdo:value",
+                                      v['scidata_key']: v['scidata_value'],
+                                      '#': v['scidata_group_link']
+                                      })
+            datum = ({
+                "@id": "datum",
+                "@type": "sdo:" + sdsub,
+                "value": value})
+            datumset.append(datum)
+        dataset = {
+            "@id": "datapoint",
+            "@type": "sdo:datapoint",
+            "data": datumset}
+        datasetmod.append(dataset)
+    return datasetmod
+
+
+def scilinker(sci_input, sci_links):
+    """Links datapoint to corresponding aspect(s) and facet(s)"""
+    inputsets = [
+        sci_input['@graph']['scidata'].get('dataset', {}).get('datapoint'),
+        sci_input['@graph']['scidata'].get('methodology', {}).get('aspects'),
+        sci_input['@graph']['scidata'].get('system', {}).get('facets')
+    ]
+
+    def reg_replace(find, zin, zbin, k, sci_bin_reg):
+        if re.search(find, zin):
+            if sci_bin_reg.get(k):
+                if type(sci_bin_reg.get(k)) == list:
+                    sci_bin_reg[k].append(zbin['@id'])
+                    sci_bin_reg[k] = list(set(sci_bin_reg[k]))
+                else:
+                    makelist = [sci_bin_reg.get(k)]
+                    sci_bin_reg[k] = makelist
+                    sci_bin_reg[k].append(zbin['@id'])
+                    sci_bin_reg[k] = list(set(sci_bin_reg[k]))
+                    if len(sci_bin_reg[k]) == 1:
+                        sci_bin_reg[k] = zbin['@id']
+            else:
+                sci_bin_reg[k] = zbin['@id']
+
+    def scilinkerloop(newput, sciloop_links, sci_bin_loop):
+        """Iterative solution for scilinker"""
+        for sci_from, sci_to in sciloop_links.items():
+            if re.search(sci_from, newput):
+                for k, v in sci_to.items():
+                    try:
+                        linkval = re.match(sci_from, newput).group(2)
+                        find = v.replace('$!@%', linkval)
+                    except Exception:
+                        find = v
+                    for inp_loop in inputsets:
+                        if inp_loop:
+                            for zbin in inp_loop:
+                                zy = zbin.get('#', False)
+                                if zy:
+                                    reg_replace(find, zy, zbin, k, sci_bin_loop)
+                                zbin_data = zbin.get('data', False)
+                                if zbin_data:
+                                    for zbin_data_entry in zbin_data:
+                                        zbin_value = zbin_data_entry.get('value', {}).get('#', False)
+                                        if zbin_value:
+                                            reg_replace(find, zbin_value, zbin, k, sci_bin_loop)
+
+    for inp in inputsets:
+        if inp:
+            for sci_bin in inp:
+                y = sci_bin.get('#', False)
+                if y:
+                    scilinkerloop(y, sci_links, sci_bin)
+                bin_data = sci_bin.get('data', False)
+                if bin_data:
+                    for bin_data_entry in bin_data:
+                        bin_value = bin_data_entry.get('value', {}).get('#', False)
+                        if bin_value:
+                            scilinkerloop(bin_value, sci_links, sci_bin)
+
+
+def scicleanup(sci_input):
+    """Links datapoint to corresponding aspect(s) and facet(s)"""
+    inputsets = [
+        sci_input['@graph']['scidata'].get('dataset', {}).get('datapoint'),
+        sci_input['@graph']['scidata'].get('methodology', {}).get('aspects'),
+        sci_input['@graph']['scidata'].get('system', {}).get('facets')
+    ]
+    for inp in inputsets:
+        if inp:
+            for sci_bin in inp:
+                y = sci_bin.get('#', False)
+                if y:
+                    sci_bin.pop('#')
+                bin_data = sci_bin.get('data', False)
+                if bin_data:
+                    for bin_data_entry in bin_data:
+                        bin_value = bin_data_entry.get('value', {}).get('#', False)
+                        if bin_value:
+                            bin_data_entry['value'].pop('#')
